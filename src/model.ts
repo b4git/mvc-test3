c4b52ee9d766d89bd3fa32b346f7db299d01345a
b4git <63267629+b4git@users.noreply.github.com>
2020-06-01 22:47:56 -0500

    update test

--- src/model.ts
+++ src/model.ts
@@ -2,6 +2,14 @@ export interface SinglesRowData {
   bid: number;
   ask: number;
   strike: number;
+  vlm: number;
+  vlt: number;
 }
 
-export const sampleRow: SinglesRowData = { bid: 1, ask: 2, strike: 300 };
+export const sampleRow: SinglesRowData = {
+  bid: 1,
+  ask: 2,
+  vlm: 200,
+  vlt: 10.15,
+  strike: 300
+};
